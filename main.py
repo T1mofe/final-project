420ae6e3dc22cf63384f44deccfac47408e76c8f
auto login <autologin@ib-410-08.ipa.psati.ru>
2023-09-30 16:16:08 +0300

    Функция на старт таймера и его конец

---
+++ main.py
@@ -0,0 +1,4 @@
+import service
+print("RED")
+print(service.start_timer())
+print("GREEN")
---
+++ service.py
@@ -0,0 +1,14 @@
+import random
+import time 
+
+
+def random_number():
+    return random.randint(2,10)
+
+
+def start_timer():
+    start_time = random_number()
+    for i in range(start_time):
+        time.sleep(1)
+
+    
