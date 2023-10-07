982cc0c08a3a66f0c10e573c96fdbe204a346097
auto login <autologin@ib-410-08.ipa.psati.ru>
2023-10-07 16:15:58 +0300

    Тест на реакцию

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
 import service
-print("RED")
-print(service.start_timer())
-print("GREEN")
+from tkinter import *
+import time
+
+class Reaction:
+    def __init__(self) -> None:
+        self.start_program()
+
+    
+    def btn_click(self):   
+        service.start_timer()
+        self.frame.config(bg='green')
+        
+        self.start_time = time.time()
+        self.frame.bind('<Button-1>', self.click)
+
+    def click(self, event):
+        self.end_time= time.time()
+        self.frame.unbind('<Button-1>')
+        self.frame.config(bg='red')
+        self.label.config(text=f'Секунды: {round(self.end_time - self.start_time, 3)}')
+
+    def start_program(self):
+        self.speed = Tk()
+
+        self.speed['bg'] = '#fafafa'
+        self.speed.title('Тест на реакцию')
+        self.speed.wm_attributes('-alpha', 0.7)
+        self.speed.geometry('500x420')
+
+        self.speed.resizable(width=False, height=False)
+
+        self.frame = Frame(self.speed, bg = 'red', height=420)
+
+        self.btn = Button(self.speed, text='Начать', bg ='grey', command=self.btn_click)
+        self.btn.pack()
+
+        self.label = Label(self.speed, text='')
+        self.label.pack()
+
+        self.frame.pack(side='top', fill='both')
+
+
+        self.speed.mainloop()
+
+program = Reaction()
--- service.py
+++ service.py
@@ -10,5 +10,4 @@ def start_timer():
     start_time = random_number()
     for i in range(start_time):
         time.sleep(1)
-
-    
+        
