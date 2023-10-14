f918fe4f84521e6b2dacb8fefbc8af16daef2602
auto login <autologin@ib-410-08.ipa.psati.ru>
2023-10-14 16:11:17 +0300

    Тест на реакцию v2.0

--- main.py
+++ main.py
@@ -2,27 +2,33 @@ from tkinter import *
 import time
 from random import randint
 
+#создаем класс для удобства
 class Reaction:
     def __init__(self) -> None:
         self.start_program()
 
+#создаем функцию для старта
     def start_game(self):
         self.frame.config(bg='red')
         self.start_time = time.time()
         self.frame.bind('<Button-1>', self.click)
     
+#по нажатию левой кнопки мыши у нас начинается отсчет до начала ,если мы нажмем слишком рано  , то функия прервется  
     def btn_click(self):
         self.label.config(text='...')
         self.cancel = self.speed.after(randint(1000, 5000), self.start_game)
         self.frame.bind('<Button-1>', self.error_click)
 
+#просто функия тестом, если нажали слишком рано 
     def error_click(self, event):
         self.label.config(text=f'Вы кликнули слишком рано! Попробуйте снова.')
 
+#функция для того чтобы программа отменялась ,если нажали слишком рано
         if self.cancel is not None:
             self.speed.after_cancel(self.cancel)
             self.cancel = None
 
+#функция дял рссчета секунд после нажатия на зеленый фон
     def click(self, event):
         self.end_time= time.time()
         self.frame.unbind('<Button-1>')
